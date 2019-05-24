0151e40a1f3cfd6bf5bdb7a83ccf34072ede1e66
sbein <sbein@umich.edu>
2019-05-24 17:42:47 -0400

    Created countdown logic, todo: update design

--- screens/Countdown.js
+++ screens/Countdown.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import { View, Text, AsyncStorage } from 'react-native';
-import TimerCountdown from "react-native-timer-countdown";
+import CountDown from 'react-native-countdown-component';
 import { SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
 import Button from '../components/Button';
 
 class CountdownScreen extends React.Component {
+    // Generate countdown start time relative to event time
+    getCountDownStartValue() {
+        if (Date.parse(this.props.configuration.start_date) < Date.now()) {
+            if(Date.now() < Date.parse(this.props.configuration.end_date)){   // Event happening
+                let t = Date.parse(this.props.configuration.end_date) - Date.now();  // get difference in milliseconds
+                let seconds  = t / 1000;  // milliseconds to seconds
+                return seconds;
+            } else {  // Event over
+                return 0;
+            }
+
+        } else {
+            // Event starting in future
+            let t = Date.parse(this.props.configuration.start_date) - Date.now();  // get difference in milliseconds
+            let seconds  = t / 1000;  // milliseconds to seconds
+            return seconds;
+        }
+    }
+    
+    // Generate text greeting relative to event time
+    generateGreeting(){
+        if (Date.parse(this.props.configuration.start_date) < Date.now()) {
+            if(Date.now() < Date.parse(this.props.configuration.end_date)){   // Event happening
+                return <Text>Hacking Time Remaining:</Text>
+            } else {
+                return <Text>Hacking Time is Over!</Text> 
+            }
+        }
+        return <Text>Mhacks Starts in:</Text>
+    }
+
     render() {
         return (
             <SafeAreaView style={{
@@ -14,22 +45,11 @@ class CountdownScreen extends React.Component {
                 justifyContent: "center"
             }}>
                 <Text>Welcome to {this.props.configuration !== null ? this.props.configuration.app_name : 'MHacks'}!</Text>
-                <Text>Not a real countdown but:</Text>
-                <TimerCountdown
-                    initialMilliseconds={1000 * 60 * 60 * 24 * 3}
-                    style={{ fontSize: 48 }}
+                {this.generateGreeting()}
+                <CountDown
+                    until={this.getCountDownStartValue()}
+                    size={20}
                 />
-                {
-                    // Temporary solution so we can quickly
-                    // clear our app's storage
-                    __DEV__ && (
-                        <Button
-                            isLoading={false}
-                            title='Clear AsyncStorage'
-                            onPress={() => AsyncStorage.clear()}
-                        />
-                    )
-                }
             </SafeAreaView>
         );
     }
